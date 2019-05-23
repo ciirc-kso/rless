710fde01c88a55b048c5fdc8ab8f63d0fe19f9a3
Jonas Vaclavek <jonas.vaclavek@gmail.com>
2019-05-23 14:06:15 +0200

    add file headers

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,8 @@
+# Created by: Jonas Vaclavek
+# Modified by: Jonas Vaclavek
+# Modify date: 23.5.2019
+# Description: Package constants
+
 PACKAGE_NAME <- "rless"
 LESS_JS_FILENAME <- "less.js"
 RLESS_JS_FILENAME <- "rless.js"
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,8 @@
+# Created by: Jonas Vaclavek
+# Modified by: Jonas Vaclavek
+# Modify date: 23.5.2019
+# Description: Testthat config file
+
 library(testthat)
 library(rless)
 
